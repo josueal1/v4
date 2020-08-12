e9de4de2d6ad2c58b733fe88d489537410a327b5
Josue Lopez <galvez2j@gmail.com>
2020-08-12 16:30:18 -0700

    feat: Create /portfolio page, section, and content

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,62 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'gatsby';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React from 'react';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Layout } from '@components';
+import { Layout, PortfolioSection, Contact } from '@components';
 import styled from 'styled-components';
-import { theme, mixins, media, Main } from '@styles';
-const { colors, fonts, navDelay } = theme;
+import { Main } from '@styles';
 
 const StyledMainContainer = styled(Main)`
-  ${mixins.flexCenter};
-  flex-direction: column;
-`;
-const StyledTitle = styled.h1`
-  color: ${colors.green};
-  font-family: ${fonts.SFMono};
-  font-size: 12vw;
-  line-height: 1;
-  ${media.bigDesktop`font-size: 200px;`}
-  ${media.phablet`font-size: 120px;`};
-`;
-const StyledSubtitle = styled.h2`
-  font-size: 3vw;
-  font-weight: 400;
-  ${media.bigDesktop`font-size: 50px;`};
-  ${media.phablet`font-size: 30px;`};
-`;
-const StyledHomeButton = styled(Link)`
-  ${mixins.bigButton};
-  margin-top: 40px;
+  counter-reset: section;
 `;
 
-const NotFoundPage = ({ location }) => {
-  const [isMounted, setIsMounted] = useState(false);
+const PortfolioPage = ({ location, data }) => (
+  <Layout location={location}>
+    <StyledMainContainer className="fillHeight">
+      <PortfolioSection data={data.portfolio.edges} />
+      <Contact data={data.contact.edges} />
+    </StyledMainContainer>
+  </Layout>
+);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => setIsMounted(true), navDelay);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  return (
-    <Layout location={location}>
-      <TransitionGroup component={null}>
-        {isMounted && (
-          <CSSTransition timeout={500} classNames="fade">
-            <StyledMainContainer className="fillHeight">
-              <StyledTitle>404</StyledTitle>
-              <StyledSubtitle>Page Not Found</StyledSubtitle>
-              <StyledHomeButton to="/">Go Home</StyledHomeButton>
-            </StyledMainContainer>
-          </CSSTransition>
-        )}
-      </TransitionGroup>
-    </Layout>
-  );
-};
-
-NotFoundPage.propTypes = {
+PortfolioPage.propTypes = {
   location: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
 };
 
-export default NotFoundPage;
+export default PortfolioPage;
+
+export const pageQuery = graphql`
+  {
+    portfolio: allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/portfolio/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            skills
+            pic1 {
+              childImageSharp {
+                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+            pic2 {
+              childImageSharp {
+                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+            pic3 {
+              childImageSharp {
+                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
+                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    contact: allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/contact/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            buttonText
+          }
+          html
+        }
+      }
+    }
+  }
+`;
